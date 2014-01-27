c0a4ac550f3f2a3993bb2e22c181a465ea6b035d
Manny <manuel_igop@yahoo.com>
2014-01-28 00:23:51 +0800

    configuration change

--- conf/conf.js
+++ conf/conf.js
@@ -1,25 +1,20 @@
 /*
  Global variables:
  global.isProd - checks if running in openshift server
- connections.mainDb - main mongoose db
- models.[model name] - returns a Mongoose Model
+ connections.mainDb - main mongoose db, convenience for oils.connections.mainDb
+ models.[model name] - returns a Mongoose Model, convenience for oils.models
  oils.isDebug - setting to true usually means more logging
 */
 
 module.exports = {
+	ipAddress: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+	port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
 	connections: {
-		//later support for multiple mongoose databaes
+		//later support for multiple mongoose databases
 		mainDb : {
-			url: getDbUrl()
+			url: (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || 'mongodb://localhost/test'
 		}
 	},
-	debug: false //oils.isDebug
+	debug: false
 }
 
-function getDbUrl() {
-	if (global.isProd) {
-	  return process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME;
-	} else {
-	  return 'mongodb://localhost/test';
-	}
-}
