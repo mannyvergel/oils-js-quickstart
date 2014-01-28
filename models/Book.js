d1b4ae3c932b02f203a0e02c43c9a87dddc7bc16
Manny <manuel_igop@yahoo.com>
2014-01-28 13:49:59 +0800

    different connection support for models

--- models/Book.js
+++ models/Book.js
@@ -4,10 +4,17 @@ module.exports = {
 		author: String,
 		title: String,
 		publishDate: Date
-	},
+	}
 
+	/*** Optional attributes
+
+	,
 	initSchema: function(schema) {
 		//initialize the schema if needed, else this
 		//methos is optional
-	}
+	},
+
+	connection: 'mainDb' //defaults to 'mainDb' or the first defined connection in conf.js
+
+	***/
 }
